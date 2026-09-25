be4431b1fd17d9a07f86268bb67caf8dee799fdd
rhit-turner <turneras@rose-hulman.edu>
2026-09-24 22:08:17 -0400

    finished portfolio

--- portfolio.js
+++ portfolio.js
@@ -3,7 +3,7 @@ let projDetails = document.getElementById("projDetails");
 
 detailsButton.addEventListener("click", function(){
     projDetails.removeAttribute("hidden");
-    detailsButton.textContent = "More Details Showing?";
+    detailsButton.textContent = "More Details Showing";
 })
 
 let detailsButton2 = document.getElementById("detailsButton2");
@@ -11,13 +11,5 @@ let projDetails2 = document.getElementById("projDetails2");
 
 detailsButton2.addEventListener("click", function(){
     projDetails2.removeAttribute("hidden");
-    detailsButton2.textContent = "More Details Showing?";
-})
-
-let detailsButton3 = document.getElementById("detailsButton3");
-let projDetails3 = document.getElementById("projDetails3");
-
-detailsButton3.addEventListener("click", function(){
-    projDetails3.removeAttribute("hidden");
-    detailsButton3.textContent = "More Details Showing???";
+    detailsButton2.textContent = "More Details Showing";
 })
